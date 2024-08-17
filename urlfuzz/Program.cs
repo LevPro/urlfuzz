6e9f3fff4e3d6b72a938a79f66a6ff2cb2d43b28
Егор Левин <info@levpro.ru>
2024-08-18 02:21:12 +0300

    Добавьте файлы проекта.

---
+++ urlfuzz/Program.cs
@@ -0,0 +1,66 @@
+﻿using urlfuzz.src.Utils;
+
+namespace urlfuzz
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: urlfuzz.exe <wordlist> <url>");
+                return;
+            }
+
+            Console.WriteLine("Load Http module...");
+
+            Http http = new();
+
+            Console.WriteLine("Load wordlist...");
+
+            try
+            {
+                // Read the contents of the file
+                string fileContents = File.ReadAllText(args[0]);
+
+                // Split the file contents into an array
+                string[] words = fileContents.Split('\n');
+
+                Console.WriteLine("Fuzzing...");
+
+                List<Task<int>> tasks = new List<Task<int>>();
+
+                foreach (string word in words)
+                {
+                    tasks.Add(Task.Run(async () =>
+                    {
+                        string url = args[1].TrimEnd('/') + '/' + word.TrimStart('/');
+
+                        Console.WriteLine("Fuzzing: " + url);
+
+                        HttpResponseMessage response = await http.Get(url);
+
+                        return (int)response.StatusCode;
+                    }));
+                }
+
+                await Task.WhenAll(tasks);
+
+                foreach (Task<int> task in tasks)
+                {
+                    int taskCode = await task;
+                    if (taskCode == 200)
+                    {
+                        Console.WriteLine("Found: " + taskCode);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred while loading the file: " + ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ urlfuzz/src/Utils/Http.cs
@@ -0,0 +1,159 @@
+﻿using System.Net;
+
+namespace urlfuzz.src.Utils
+{
+    public class Http
+    {
+        private readonly HttpClient _client;
+        private readonly CookieContainer _cookieContainer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Http"/> class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor creates a new instance of the <see cref="HttpClient"/> class and assigns it to the
+        /// <see cref="_client"/> field.
+        /// </remarks>
+        public Http()
+        {
+            _cookieContainer = new CookieContainer();
+            _client = new HttpClient(new HttpClientHandler { CookieContainer = _cookieContainer });
+        }
+
+        /// <summary>
+        /// Adds a cookie to the cookie container with the specified name, value, domain, and path.
+        /// </summary>
+        /// <param name="name">The name of the cookie.</param>
+        /// <param name="value">The value of the cookie.</param>
+        /// <param name="domain">The domain of the cookie.</param>
+        /// <param name="path">The path of the cookie.</param>
+
+        public void AddCookie(string name, string value, string domain, string path)
+        {
+            _cookieContainer.Add(new Cookie(name, value, path, domain));
+        }
+
+        /// <summary>
+        /// Adds a header to the request.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        public void AddHeader(string name, string value)
+        {
+            _client.DefaultRequestHeaders.Add(name, value);
+        }
+
+        /// <summary>
+        /// Sets the User-Agent header.
+        /// </summary>
+        /// <param name="userAgent"></param>
+        public void SetUserAgent(string userAgent) => AddHeader("User-Agent", userAgent);
+
+        /// <summary>
+        /// Sets the request timeout.
+        /// </summary>
+        /// <param name="timeout"></param>
+        public void SetTimeout(int timeout) => _client.Timeout = TimeSpan.FromSeconds(timeout);
+
+        /// <summary>
+        /// Sends a PUT request to the specified URL with the specified data.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public async Task<HttpResponseMessage> Put(string url, Dictionary<string, string> data)
+        {
+            return await _client.PutAsync(url, new FormUrlEncodedContent(data));
+        }
+
+        /// <summary>
+        /// Sends a POST request to the specified URL with the specified data.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public async Task<HttpResponseMessage> Post(string url, Dictionary<string, string> data)
+        {
+            return await _client.PostAsync(url, new FormUrlEncodedContent(data));
+        }
+
+        /// <summary>
+        /// Sends a GET request to the specified URL.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public async Task<HttpResponseMessage> Get(string url)
+        {
+            return await _client.GetAsync(url);
+        }
+
+        /// <summary>
+        /// Sends a DELETE request to the specified URL.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public async Task<HttpResponseMessage> Delete(string url)
+        {
+            return await _client.DeleteAsync(url);
+        }
+
+        /// <summary>
+        /// Sends a HEAD request to the specified URL.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public async Task<HttpResponseMessage> Head(string url)
+        {
+            return await _client.SendAsync(new HttpRequestMessage(HttpMethod.Head, url));
+        }
+
+        /// <summary>
+        /// Sends a PATCH request to the specified URL with the specified data.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public async Task<HttpResponseMessage> Patch(string url, Dictionary<string, string> data)
+        {
+            return await _client.PatchAsync(url, new FormUrlEncodedContent(data));
+        }
+
+        /// <summary>
+        /// Sends a OPTIONS request to the specified URL.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public async Task<HttpResponseMessage> Options(string url)
+        {
+            return await _client.SendAsync(new HttpRequestMessage(HttpMethod.Options, url));
+        }
+
+        /// <summary>
+        /// Sends a TRACE request to the specified URL.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public async Task<HttpResponseMessage> Trace(string url)
+        {
+            return await _client.SendAsync(new HttpRequestMessage(HttpMethod.Trace, url));
+        }
+
+        /// <summary>
+        /// Reads the response from the HTTP response.
+        /// </summary>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        private async Task<string> ReadResponseString(HttpResponseMessage response)
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                throw new Exception($"Request failed with status code: {response.StatusCode}");
+            }
+        }
+    }
+}
